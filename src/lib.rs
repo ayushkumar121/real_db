0ab9f2992133eeba58d80ac70b81f5c8cee04d31
ayushkumar121 <ayushkumar121@outlook.com>
2022-12-05 16:26:41 +0530

    Added new keywords:
    - Filter
    - Insert

--- src/lib.rs
+++ src/lib.rs
@@ -1,140 +1,10 @@
+use query::*;
 use random::Source;
-use std::{collections::HashMap, fs};
+use std::collections::HashMap;
+use std::fs;
+use std::rc::Rc;
 
-#[derive(Debug)]
-struct Token {
-    word: String,
-    kind: TokenKind,
-}
-
-#[derive(Debug)]
-enum TokenKind {
-    Set,
-    Select,
-    Auto,
-    Int,
-    Float,
-    String,
-    Word,
-}
-
-fn match_token_kind(word: &str) -> TokenKind {
-    let word = word.to_lowercase();
-    match word.as_str() {
-        "_" => TokenKind::Auto,
-        "set" => TokenKind::Set,
-        "select" => TokenKind::Select,
-        _ => {
-            if word.starts_with('\"') && word.ends_with('\"') {
-                return TokenKind::String;
-            }
-
-            if word.parse::<i64>().is_ok() {
-                return TokenKind::Int;
-            }
-
-            if word.parse::<f64>().is_ok() {
-                return TokenKind::Float;
-            }
-
-            TokenKind::Word
-        }
-    }
-}
-
-fn parse_file(file_name: &str) -> Result<Vec<Token>, String> {
-    let contents = match fs::read_to_string(file_name) {
-        Ok(val) => val,
-        Err(e) => return Err(e.kind().to_string()),
-    };
-
-    fn flush_token(tokens: &mut Vec<Token>, word: &mut String) {
-        let w = word.trim();
-
-        if w.is_empty() {
-            return;
-        }
-
-        tokens.push(Token {
-            word: w.to_owned(),
-            kind: match_token_kind(w),
-        });
-
-        word.clear();
-    }
-
-    let mut tokens = Vec::new();
-    let mut word = String::new();
-    let mut is_str = false;
-
-    for ch in contents.chars() {
-        match ch {
-            ' ' | '\n' | '\r' => {
-                if !is_str {
-                    flush_token(&mut tokens, &mut word);
-                }
-            }
-            '\"' => {
-                is_str = !is_str;
-            }
-            _ => {}
-        }
-
-        word.push(ch);
-    }
-
-    flush_token(&mut tokens, &mut word);
-    Ok(tokens)
-}
-
-type Id = u64;
-
-#[derive(Debug)]
-enum Value {
-    Id(Id),
-    Int(i64),
-    Float(f64),
-    String(String),
-}
-
-enum Operation {
-    Push(Value),
-    Set,
-    Select,
-}
-
-type Program = Vec<Operation>;
-
-fn parse_program(tokens: Vec<Token>) -> Result<Program, String> {
-    let mut program = Vec::new();
-    let mut source = random::default(42);
-
-    for token in tokens {
-        match token.kind {
-            TokenKind::String => {
-                let mut w = token.word.strip_prefix('\"').unwrap();
-                w = w.strip_suffix('\"').unwrap();
-
-                program.push(Operation::Push(Value::String(w.to_owned())));
-            }
-            TokenKind::Int => {
-                program.push(Operation::Push(Value::Int(token.word.parse().unwrap())))
-            }
-            TokenKind::Float => {
-                program.push(Operation::Push(Value::Float(token.word.parse().unwrap())))
-            }
-            TokenKind::Auto => {
-                let n: Id = source.read_u64();
-                program.push(Operation::Push(Value::Id(n)));
-            }
-            TokenKind::Set => program.push(Operation::Set),
-            TokenKind::Select => program.push(Operation::Select),
-            _ => return Err(format!("unknown word `{}`", token.word).to_owned()),
-        }
-    }
-
-    Ok(program)
-}
+mod query;
 
 #[derive(Debug)]
 struct Field {
@@ -147,9 +17,13 @@ struct Record {
     fields: Vec<Field>,
 }
 
-fn execute_program(program: Program) -> Result<(), String> {
+type Records = HashMap<Id, Rc<Record>>;
+
+fn execute_program(program: Program) -> Result<Records, String> {
     let mut stack = Vec::new();
-    let mut records: HashMap<Id, Record> = HashMap::new();
+    let mut records: Records = HashMap::new();
+    let mut result: Records = HashMap::new();
+    let mut rng = random::default(42);
 
     for op in program {
         match op {
@@ -177,13 +51,42 @@ fn execute_program(program: Program) -> Result<(), String> {
                 let new_field = Field { key, value };
 
                 if let Some(record) = records.get_mut(&record_id) {
+                    let record = Rc::get_mut(record).unwrap();
                     record.fields.push(new_field);
                 } else {
                     records.insert(
                         record_id,
-                        Record {
+                        Rc::new(Record {
                             fields: vec![new_field],
-                        },
+                        }),
+                    );
+                }
+            }
+            Operation::Insert => {
+                // stack must contain 3 values
+                //  Key, Value
+                if stack.len() < 2 {
+                    return Err(
+                        "Stack must have atleast 2 values, current stack is {:#?}".to_owned()
+                    );
+                }
+                let value = stack.pop().unwrap();
+                let key = match stack.pop().unwrap() {
+                    Value::String(str) => str,
+                    _ => return Err("Key must be a string".to_owned()),
+                };
+                let new_field = Field { key, value };
+                let record_id: Id = rng.read_u64();
+
+                if let Some(record) = records.get_mut(&record_id) {
+                    let record = Rc::get_mut(record).unwrap();
+                    record.fields.push(new_field);
+                } else {
+                    records.insert(
+                        record_id,
+                        Rc::new(Record {
+                            fields: vec![new_field],
+                        }),
                     );
                 }
             }
@@ -195,6 +98,7 @@ fn execute_program(program: Program) -> Result<(), String> {
                         "Stack must have atleast 1 values, current stack is {:#?}".to_owned()
                     );
                 }
+
                 let record_id = match stack.pop().unwrap() {
                     Value::Id(id) => id,
                     Value::Int(num) => num as Id,
@@ -202,25 +106,71 @@ fn execute_program(program: Program) -> Result<(), String> {
                 };
 
                 if let Some(record) = records.get(&record_id) {
-                    println!("**********************");
-                    println!("Id={:#?}", record_id);
-                    for field in &record.fields {
-                        println!("{}={:#?}", field.key, field.value);
-                    }
-                    println!("**********************");
+                    result.insert(record_id, record.clone());
                 } else {
                     return Err("Record not found".to_owned());
                 }
             }
+            Operation::SelectAll => {
+                for (record_id, record) in &records {
+                    result.insert(*record_id, record.clone());
+                }
+            }
+            Operation::Filter => {
+                // stack must contain 3 values
+                // Key, Value, Predicate
+                if stack.len() < 3 {
+                    return Err(
+                        "Stack must have atleast 3 values, current stack is {:#?}".to_owned()
+                    );
+                }
+
+                let _predicate = match stack.pop().unwrap() {
+                    Value::String(predicate) => match predicate.as_str() {
+                        "==" => predicate,
+                        _ => return Err(format!("Unknown predicated `{}`", predicate).to_owned()),
+                    },
+                    _ => return Err("Record Id must be an id".to_owned()),
+                };
+                let value = stack.pop().unwrap();
+                let key = match stack.pop().unwrap() {
+                    Value::String(str) => str,
+                    _ => return Err("Key must be a string".to_owned()),
+                };
+
+                for (record_id, record) in &records {
+                    let mut include = false;
+                    for field in &record.fields {
+                        if field.key == key && field.value == value {
+                            include = true;
+                            break;
+                        }
+                    }
+
+                    if include {
+                        result.insert(*record_id, record.clone());
+                    }
+                }
+            }
         }
     }
-    Ok(())
+
+    Ok(result)
 }
 
 pub fn run() -> Result<(), String> {
-    let tokens = parse_file("hello.real")?;
-    let program = parse_program(tokens)?;
-    execute_program(program)?;
+    let contents = fs::read_to_string("hello.real").unwrap();
+    let program = query::parse_program(contents)?;
+    let result = execute_program(program)?;
+
+    for (record_id, record) in &result {
+        println!("**********************");
+        println!("Id={:#?}", record_id);
+        for field in &record.fields {
+            println!("{}={:#?}", field.key, field.value);
+        }
+        println!("**********************");
+    }
 
     Ok(())
 }
---
+++ src/query.rs
@@ -0,0 +1,168 @@
+use random::Source;
+
+#[derive(Debug)]
+pub struct Token {
+    word: String,
+    kind: TokenKind,
+    line: usize,
+    col: usize,
+}
+
+#[derive(Debug)]
+pub enum TokenKind {
+    Set,
+    Insert,
+    Select,
+    SelectAll,
+    Filter,
+    Auto,
+    Int,
+    Float,
+    String,
+    Word,
+}
+
+fn match_token_kind(word: &str) -> TokenKind {
+    let word = word.to_lowercase();
+    match word.as_str() {
+        "_" => TokenKind::Auto,
+        "set" => TokenKind::Set,
+        "insert" => TokenKind::Insert,
+        "select" => TokenKind::Select,
+        "select_all" => TokenKind::SelectAll,
+        "filter" => TokenKind::Filter,
+        _ => {
+            if word.starts_with('\"') && word.ends_with('\"') {
+                return TokenKind::String;
+            }
+
+            if word.parse::<i64>().is_ok() {
+                return TokenKind::Int;
+            }
+
+            if word.parse::<f64>().is_ok() {
+                return TokenKind::Float;
+            }
+
+            TokenKind::Word
+        }
+    }
+}
+
+fn tokenize(contents: String) -> Result<Vec<Token>, String> {
+    fn flush_token(tokens: &mut Vec<Token>, word: &mut String, line: usize, col: usize) {
+        let w = word.trim();
+
+        if w.is_empty() {
+            return;
+        }
+
+        tokens.push(Token {
+            word: w.to_owned(),
+            kind: match_token_kind(w),
+            line,
+            col,
+        });
+
+        word.clear();
+    }
+
+    let mut tokens = Vec::new();
+    let mut word = String::new();
+    let mut is_str = false;
+    let mut is_comment = false;
+    let mut line = 1;
+    let mut col = 1;
+
+    for ch in contents.chars() {
+        match ch {
+            ' ' | '\n' | '\r' => {
+                if !is_str {
+                    flush_token(&mut tokens, &mut word, line, col);
+                }
+
+                if ch == '\n' {
+                    is_comment = false;
+                    line += 1;
+                    col = 0;
+                }
+            }
+            '#' => is_comment = true,
+            '\"' => {
+                is_str = !is_str;
+            }
+            _ => {}
+        }
+
+        if !is_comment {
+            word.push(ch);
+            col += 1;
+        }
+    }
+
+    flush_token(&mut tokens, &mut word, line, col);
+    Ok(tokens)
+}
+
+pub type Id = u64;
+
+#[derive(Debug, PartialEq)]
+pub enum Value {
+    Id(Id),
+    Int(i64),
+    Float(f64),
+    String(String),
+}
+
+pub enum Operation {
+    Push(Value),
+    Set,
+    Select,
+    SelectAll,
+    Filter,
+    Insert,
+}
+
+pub type Program = Vec<Operation>;
+
+pub fn parse_program(contents: String) -> Result<Program, String> {
+    let tokens = tokenize(contents)?;
+
+    let mut program = Vec::new();
+    let mut rng = random::default(42);
+
+    for token in tokens {
+        match token.kind {
+            TokenKind::String => {
+                let mut w = token.word.strip_prefix('\"').unwrap();
+                w = w.strip_suffix('\"').unwrap();
+
+                program.push(Operation::Push(Value::String(w.to_owned())));
+            }
+            TokenKind::Int => {
+                program.push(Operation::Push(Value::Int(token.word.parse().unwrap())))
+            }
+            TokenKind::Float => {
+                program.push(Operation::Push(Value::Float(token.word.parse().unwrap())))
+            }
+            TokenKind::Auto => {
+                let n: Id = rng.read_u64();
+                program.push(Operation::Push(Value::Id(n)));
+            }
+            TokenKind::Set => program.push(Operation::Set),
+            TokenKind::Select => program.push(Operation::Select),
+            TokenKind::SelectAll => program.push(Operation::SelectAll),
+            TokenKind::Filter => program.push(Operation::Filter),
+            TokenKind::Insert => program.push(Operation::Insert),
+            _ => {
+                return Err(format!(
+                    "unknown word `{}` at line {}:{}",
+                    token.word, token.line, token.col
+                )
+                .to_owned())
+            }
+        }
+    }
+
+    Ok(program)
+}
